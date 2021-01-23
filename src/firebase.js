b078823e8e9d2ddb88e3a8caccef097159f092db
apekhnik <iam3ji@gmail.com>
2021-01-23 19:39:59 +0300

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD6884f0Tbhn0Sl-Vz60UgLd9YmWj2FJO0",
+  authDomain: "linkedin-clone-8fcf0.firebaseapp.com",
+  projectId: "linkedin-clone-8fcf0",
+  storageBucket: "linkedin-clone-8fcf0.appspot.com",
+  messagingSenderId: "885829369408",
+  appId: "1:885829369408:web:f5560500c1c4a17d3569a5",
+};
